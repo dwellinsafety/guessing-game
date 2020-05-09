ab7511e8cbe4979733f2f975b039e45034fd2d98
Matthew Zenaldin <mxz5066@gmail.com>
2020-05-09 16:26:46 -0700

    code for guessing game

--- guess.py
+++ guess.py
@@ -1,1 +1,21 @@
+from random import randint
 
+num=randint(1,10)
+
+print("Welcome to the guessing game!")
+
+guess = input("\nGuess an integer between 1 and 10.")
+guess = int(guess)
+
+def main():
+	while True:
+		if guess is not num:
+			guess = input("Guess again: ")
+			guess = int(guess)
+		else:
+			print("You guessed correctly!")
+			break
+
+if __name__ == '__main__':
+	main()
+	
